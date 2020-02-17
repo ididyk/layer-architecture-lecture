763eda3823023f7f2b6b0f7ca749c7d74502cc8c
Ihor Didyk <ihordidyk95@gmail.com>
2020-02-17 23:12:34 +0200

    created project

---
+++ src/main/java/edu/cursor/LayeredArchitectureApplication.java
@@ -0,0 +1,13 @@
+package edu.cursor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayeredArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LayeredArchitectureApplication.class, args);
+    }
+
+}
